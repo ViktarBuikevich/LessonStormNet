2f65ad1f2f1a50b67a7bc764190e17c62d77a4c6
Viktar.Buikevich <bulbash77@gmail.com>
2022-11-21 07:38:43 +0300

    Add file .gitignore

--- src/Lessons/L23_Thread20220920/Skillbox/imageResizer/Main.java
+++ src/Lessons/L23_Thread20220920/Skillbox/imageResizer/Main.java
@@ -9,6 +9,12 @@ public class Main {
         File srcDir = new File(srcFolder);
         long start = System.currentTimeMillis();
         File[] files = srcDir.listFiles();
+        Runtime runtime = Runtime.getRuntime();
+        int nrOfProcessors = runtime.availableProcessors();
+        System.out.println("Number of processors available to the Java Virtual Machine: " + nrOfProcessors);
+
+
+
 
     }
 }
